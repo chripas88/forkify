1530465702aad21a9fbcd2d4720705d2fc139662
chripas88 <chri-pas@hotmail.com>
2020-03-09 13:22:20 +0000

    search results

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import num from './test';
-const x = 33;
-console.log(`I imported ${num} from another module. Var x = ${x}.`);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/js/index.js'
   ],
   // output: {
   //   path: path.resolve(__dirname, 'dist'),
